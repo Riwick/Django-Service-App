eca44d8520107049a0dffb7f2d60c498a058c974
Rodion <rodbel229@gmail.com>
2024-01-31 17:24:38 +0500

    Optimize first SQL requests in controllers.py and added logging in settings.py

--- service/services/busibess_logic/controllers.py
+++ service/services/busibess_logic/controllers.py
@@ -1,5 +1,10 @@
+from django.db.models import Prefetch
+
+from clients.models import Client
 from services.models import Subscription
 
 
 def get_subscription_model():
-    return Subscription.objects.all()
+    return Subscription.objects.all().prefetch_related(
+        Prefetch('client', queryset=Client.objects.all().select_related('user').
+                 only('company_name', 'user__email')))
