12e89b081b5bbb7938e738abc05c939f21433c99
Rodion <rodbel229@gmail.com>
2024-02-01 20:16:04 +0500

    Added first task

--- service/services/busibess_logic/controllers.py
+++ service/services/busibess_logic/controllers.py
@@ -5,11 +5,10 @@ from services.models import Subscription, Plan
 
 
 def get_subscription_model():
-    return Subscription.objects.all().prefetch_related(
+    return (Subscription.objects.all().prefetch_related(
         'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').
-                 only('company_name', 'user__email'))).annotate(
-        price=F('service__full_price') - (F('service__full_price') * F('plan__discount_percent') / 100.00)
-    )
+                 only('company_name', 'user__email'))))
+    #
 
 
